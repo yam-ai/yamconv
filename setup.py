dcff776b4b2760a90b9c973c28b836b07eee0735
Thomas Lee <thomaslee@yam.ai>
2019-09-10 17:21:19 +0800

    Revise version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='yamconv',
-    version='0.1.5',
+    version='0.1.6',
     description='yamconv converts the file formats of machine learning datasets',
     long_description=long_description,
     long_description_content_type='text/markdown',
